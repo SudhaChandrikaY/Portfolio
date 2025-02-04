0046735d5ef4b6eaeac6afe92b6cde6556d04713
SudhaChandrikaY <sudhachandrikay@gmail.com>
2025-02-04 13:57:24 -0600

    Initial commit of my Portfolio Website

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={"/assets/contact/emailIcon.png"} alt="Email icon" />
+          <a href="mailto:ysudhachandrika@gmail.com">Email me</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={"/assets/contact/linkedinIcon.png"}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/sudhachandrikay">Connect with me on LinkedIn</a>
+        </li>
+        <li className={styles.link}>
+          <img src={"/assets/contact/githubIcon.png"} alt="Github icon" />
+          <a href="https://github.com/SudhaChandrikaY">My GitHub </a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
