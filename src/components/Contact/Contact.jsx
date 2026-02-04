d93b0d88795b8bae0cb15056c3b28bc0f69fde01
SudhaChandrikaY <sudhachandrikay@gmail.com>
2026-02-04 09:11:44 -0600

    updates to contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -15,13 +15,13 @@ export const Contact = () => {
           <img src={"/assets/contact/emailIcon.png"} alt="Email icon" />
           <a href="mailto:ysudhachandrika@gmail.com">Email me</a>
         </li>
-        <li className={styles.link}>
+        {/*<li className={styles.link}>
           <img
             src={"/assets/contact/linkedinIcon.png"}
             alt="LinkedIn icon"
           />
           <a href="https://www.linkedin.com/in/sudhayadlapalli" target="_blank">Connect with me on LinkedIn</a>
-        </li>
+        </li>*/}
         <li className={styles.link}>
           <img src={"/assets/contact/githubIcon.png"} alt="Github icon" />
           <a href="https://github.com/SudhaChandrikaY" target="_blank">My GitHub </a>
